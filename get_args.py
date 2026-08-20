09b7a3f22148f8cb1383b588f8b88551a3e0c6b3
swufe-NiceLab-GeoText <nicelab_geotex@163.com>
2026-08-20 17:28:16 +0800

    Update get_args.py

--- get_args.py
+++ get_args.py
@@ -7,6 +7,7 @@ class Args:
         with open(path, "r", encoding="utf-8") as f:
             config = json.load(f)
 
+
         self.dataset = config["dataset"]
         self.train_path = config["train_path"]
         self.dev_path = config["dev_path"]
@@ -40,6 +41,7 @@ class Args:
         self.gama = config["gama"]
         self.se_layer = config["se_layer"]
         self.q_qshot = config["q_qshot"]
+        # self.margin = config["margin"]
 
         self.prompt_len = config["prompt_len"]
         self.key_hidden_size = config["key_hidden_size"]
@@ -54,115 +56,146 @@ class Args:
         self.warmup_steps = config["warmup_steps"]
         self.dropout = config["dropout"]
 
-        # LLM / LoRA settings (optional, with defaults for backward compatibility)
-        self.use_lora = config.get("use_lora", False)
-        self.lora_r = config.get("lora_r", 16)
-        self.lora_alpha = config.get("lora_alpha", 32)
-        self.lora_dropout = config.get("lora_dropout", 0.1)
-        self.fp16 = config.get("fp16", False)
+        self.optionn = "bert_layer"
+
 
         self.gama1 = 1.0
         self.gama2 = 1.0
 
         self.logger_path = "code/"
         self.logger = utils.get_logger("aaa", self.logger_path)
-        self.logger.info("Initialized config from: " + path)
+        self.logger.info("come on!")
+
+
 
     def __repr__(self):
         return "{}".format(self.__dict__.items())
 
     def show_self(self):
-        print("=" * 50)
-        print("Configuration:")
-        print("=" * 50)
-        for key in ['train_path', 'dev_path', 'test_path', 'checkpoint_path',
-                    'epochs', 'episodes', 'numFreeze', 'filevocab', 'fileModel',
-                    'gpu', 'seed', 'hidden_size', 'nway', 'kshot', 'qshot',
-                    'text_max_len', 'label_max_len', 'key_hidden_size',
-                    'pool_len', 'key_init_method', 'prompt_len',
-                    'weight_decay', 'learning_rate', 'warmup_steps']:
-            print(f"  {key}: {getattr(self, key)}")
-        print("=" * 50)
+        print("train_path: {} -----".format(self.train_path))
+        print("dev_path: {} -----".format(self.dev_path))
+        print("test_path: {} -----".format(self.test_path))
+        print("checkpoint_path: {} -----".format(self.checkpoint_path))
 
-    def write_self(self, path):
-        """Save configuration as a proper JSON file."""
-        config = {
-            'train_path': self.train_path,
-            'dev_path': self.dev_path,
-            'test_path': self.test_path,
-            'checkpoint_path': self.checkpoint_path,
-            'epochs': self.epochs,
-            'episodes': self.episodes,
-            'episodes_q': self.episodes_q,
-            'numFreeze': self.numFreeze,
-            'filevocab': self.filevocab,
-            'fileModelConfig': self.fileModelConfig,
-            'fileModel': self.fileModel,
-            'gpu': self.gpu,
-            'seed': self.seed,
-            'hidden_size': self.hidden_size,
-            'nway': self.nway,
-            'kshot': self.kshot,
-            'qshot': self.qshot,
-            'text_max_len': self.text_max_len,
-            'label_max_len': self.label_max_len,
-            'beta': self.beta,
-            'step': self.step,
-            'random_len': self.random_len,
-            'it_num': self.it_num,
-            'temprature': self.temprature,
-            'temprature1': self.temprature1,
-            'alpha': self.alpha,
-            'gama': self.gama,
-            'se_layer': self.se_layer,
-            'q_qshot': self.q_qshot,
-            'prompt_len': self.prompt_len,
-            'key_hidden_size': self.key_hidden_size,
-            'pool_len': self.pool_len,
-            'key_init_method': self.key_init_method,
-            'weight_decay': self.weight_decay,
-            'learning_rate': self.learning_rate,
-            'warmup_steps': self.warmup_steps,
-            'dropout': self.dropout,
-            'optionn': self.optionn,
-        }
-        with open(path, 'w', encoding='utf-8') as f:
-            json.dump(config, f, indent=4, ensure_ascii=False)
+        print("epochs: {} -----".format(self.epochs))
+        print("episodes: {} -----".format(self.episodes))
+        print("numFreeze: {} -----".format(self.numFreeze))
+        print("filevocab: {} -----".format(self.filevocab))
+        print("fileModelConfig: {} -----".format(self.fileModelConfig))
+        print("fileModel: {} -----".format(self.fileModel))
 
-    def update_from_args(self, cli_args):
-        """Update config from command-line arguments. Sentinel values are skipped."""
-        mapping = {
-            'gpu': ('gpu', -1),
-            'tempra1': ('temprature1', -1.0),
-            'kshot': ('kshot', -1),
-            'beta': ('beta', -1.0),
-            'temprature': ('temprature', -1.0),
-            'dataset_num': ('dataset', "09"),
-            'seed': ('seed', -1),
-            'qshot': ('qshot', -1),
-            'alpha': ('alpha', -1.0),
-            'gama': ('gama', -1.0),
-            'se_layer': ('se_layer', -1),
-            'weight_decay': ('weight_decay', 0.00),
-            'margin': ('margin', -1.0),
-            'prompt_len': ('prompt_len', -1),
-            'numFreeze': ('numFreeze', -1),
-            'pool_len': ('pool_len', -1),
-            'epochs': ('epochs', -1),
-            'warmup_steps': ('warmup_steps', -1),
-            'step': ('step', -1),
-            'learning_rate': ('learning_rate', -1.0),
-            'dropout': ('dropout', -1.0),
-            'text_len': ('text_max_len', -1),
-            'label_len': ('label_max_len', -1),
-            'gama1': ('gama1', -1.0),
-            'gama2': ('gama2', -1.0),
-        }
-        for cli_name, (attr_name, sentinel) in mapping.items():
-            cli_val = getattr(cli_args, cli_name, sentinel)
-            if cli_val != sentinel:
-                setattr(self, attr_name, cli_val)
+        print("gpu: {} -----".format(self.gpu))
+        print("seed: {} -----".format(self.seed))
+        print("hidden_size: {} -----".format(self.hidden_size))
+        print("nway: {} -----".format(self.nway))
+        print("kshot: {} -----".format(self.kshot))
+        print("qshot: {} -----".format(self.qshot))
 
-        # Always-set fields
-        self.optionn = cli_args.optionn
-        self.dataset_name = cli_args.dataset_name
+        print("text_max_len: {} -----".format(self.text_max_len))
+        print("label_max_len: {} -----".format(self.label_max_len))
+        # print("beta: {} -----".format(self.beta))
+        # print("step: {} -----".format(self.step))
+        # print("random_len: {} -----".format(self.random_len))
+        # print("it_num: {} -----".format(self.it_num))
+        print("key_hidden_size: {} -----".format(self.key_hidden_size))
+        # print("temprature1: {} -----".format(self.temprature1))
+        # print("alpha: {} -----".format(self.alpha))
+        # print("gama: {} -----".format(self.gama))
+        print("pool_len: {} -----".format(self.pool_len))
+        print("key_init_method: {} -----".format(self.key_init_method))
+
+        print("prompt_len: {} -----".format(self.prompt_len))
+        print("weight_decay: {} -----".format(self.weight_decay))
+        print("learning_rate: {} -----".format(self.learning_rate))
+        print("warmup_steps: {} -----".format(self.warmup_steps))
+
+    def write_self(self,path):
+        with open(path, 'w') as json_file:
+            data = {'train_path': self.train_path}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'dev_path': self.dev_path}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'test_path': self.test_path}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'checkpoint_path': self.checkpoint_path}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+            data = {'epochs': self.epochs}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'episodes': self.episodes}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'episodes_q': self.episodes_q}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'numFreeze': self.numFreeze}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'filevocab': self.filevocab}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'fileModelConfig': self.fileModelConfig}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'fileModel': self.fileModel}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+            data = {'gpu': self.gpu}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'seed': self.seed}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'hidden_size': self.hidden_size}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'nway': self.nway}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'kshot': self.kshot}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'qshot': self.qshot}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+            data = {'text_max_len': self.text_max_len}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'label_max_len': self.label_max_len}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'beta': self.beta}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'step': self.step}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'random_len': self.random_len}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'it_num': self.it_num}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'temprature': self.temprature}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'temprature1': self.temprature1}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'alpha': self.alpha}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'gama': self.gama}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'se_layer': self.se_layer}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'q_qshot': self.q_qshot}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+            data = {'prompt_len': self.prompt_len}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'key_hidden_size': self.key_hidden_size}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'pool_len': self.pool_len}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'key_init_method': self.key_init_method}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+            data = {'weight_decay': self.weight_decay}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'learning_rate': self.learning_rate}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'warmup_steps': self.warmup_steps}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'dropout': self.dropout}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+            data = {'optionn': self.optionn}
+            json_file.write(json.dumps(data, ensure_ascii=False) + '\n')
+
+
+
+
+
+
+
